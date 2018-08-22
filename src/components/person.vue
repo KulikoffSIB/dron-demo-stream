aa7b6051f8c170b5e146ab531a0a6c3dc7cb4eea
KulikovASe <KulikovASe@sibintek.ru>
2018-08-22 12:26:32 +0300

    верстка блока логотипов под блоком пользователя

--- src/components/person.vue
+++ src/components/person.vue
@@ -1,48 +1,59 @@
 <template>
-    <div class="person-wrap">
-        <div class="flex-container-column" style="height: 100%">
-            <div class="flex-element-3 photo">
-                <div class="flex-container-row">
-                    <div class="flex-element-1">PHOTO</div>
-                    <div class="flex-element-1">
-                        <div class="flex-container-column" style="height: 100%">
-                            <div class="flex-element-1 fio">
-                                <span class="key">Фамилия</span> <br>
-                                <span class="value">{{lastname}}</span>
-                            </div>
-                            <div class="flex-element-1 fio">
-                                <span class="key">Имя</span> <br>
-                                <span class="value">{{name}}</span>
-                            </div>
-                            <div class="flex-element-1 fio">
-                                <span class="key">Отчество</span> <br>
-                                <span class="value">{{midname}}</span>
+    <div>
+        <div class="person-wrap">
+            <div class="flex-container-column" style="height: 100%">
+                <div class="flex-element-3 photo">
+                    <div class="flex-container-row">
+                        <div class="flex-element-1">PHOTO</div>
+                        <div class="flex-element-1">
+                            <div class="flex-container-column" style="height: 100%">
+                                <div class="flex-element-1 fio">
+                                    <span class="key">Фамилия</span> <br>
+                                    <span class="value">{{lastname}}</span>
+                                </div>
+                                <div class="flex-element-1 fio">
+                                    <span class="key">Имя</span> <br>
+                                    <span class="value">{{name}}</span>
+                                </div>
+                                <div class="flex-element-1 fio">
+                                    <span class="key">Отчество</span> <br>
+                                    <span class="value">{{midname}}</span>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="flex-element-1 user-data">
-                <div class="flex-container-row" style="height: 100%">
-                    <div class="flex-element-1 fio">
-                        <span class="key">Компания</span> <br>
-                        <span class="value">{{company}}</span>
-                    </div>
-                    <div class="flex-element-1 fio">
-                        <span class="key">Должность</span> <br>
-                        <span class="value">{{post}}</span>
+                <div class="flex-element-1 user-data">
+                    <div class="flex-container-row" style="height: 100%">
+                        <div class="flex-element-1 fio">
+                            <span class="key">Компания</span> <br>
+                            <span class="value">{{company}}</span>
+                        </div>
+                        <div class="flex-element-1 fio">
+                            <span class="key">Должность</span> <br>
+                            <span class="value">{{post}}</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="flex-element-1 event">
-                <span v-if="inHelmet == null">Проверка наличия каски...</span>
-                <div v-if="inHelmet != null">
-                    <div v-if="inHelmet">В каске</div>
-                    <div v-else>Без каски</div>
+                <div class="flex-element-1 event">
+                    <span v-if="inHelmet == null">Проверка наличия каски...</span>
+                    <div v-if="inHelmet != null">
+                        <div v-if="inHelmet">В каске</div>
+                        <div v-else>Без каски</div>
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="logo-wrap">
+            <div class="logo rosneft">
+                <img src="../../src/assets/RN.png" alt="ROSNEFT">
+            </div>
+            <div class="logo sibintek">
+                <img src="../../src/assets/SIB.png" alt="SIBINTEK">
+            </div>
+        </div>
     </div>
+
 </template>
 
 <script>
@@ -67,13 +78,29 @@
 </script>
 
 <style scoped>
+    img {
+        height: auto;
+        width: 140px;
+
+    }
+
     .person-wrap {
         margin-left: 40px;
         width: 400px;
-        height: 812px;
+        height: 512px;
         border: 1px solid #57A1D0;
         border-top: 4px solid #57A1D0;
         background-color: #0D2C49;
+    }
+
+    .logo-wrap {
+        height: 200px;
+        margin-left: 70px;
+        margin-top: 100px;
+    }
+
+    .logo {
+        height: 160px
     }
 
     .border-warning {
